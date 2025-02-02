0048a6261bdece36c4488e774115b4138a60a2ce
namrata0226 <namratakurariya02@gmail.com>
2025-02-02 23:03:58 +0530

    adding shimmer ui

---
+++ frontend/src/components/Shimmer.jsx
@@ -0,0 +1,18 @@
+import './shimmer.css';
+const Shimmer = () => {
+  return (
+   
+     <div className="loader">
+  <div className="wrapper">
+    
+    <div className="line-1"></div>
+    <div className="line-2"></div>
+    <div className="line-3"></div>
+  
+  </div>
+
+    </div>
+  );
+};
+
+export default Shimmer;
--- frontend/src/components/UserTable.jsx
+++ frontend/src/components/UserTable.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import Shimmer from "./Shimmer";
 
 const UserTable = () => {
   const [users, setUsers] = useState([]);
+  const [loader, setLoader] = useState(true);
+
   const navigate = useNavigate();
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BASE_URL}/`)
       .then((res) => res.json())
-      .then((data) => setUsers(data))
+      .then((data) => {
+        setUsers(data);
+        setLoader(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -24,94 +30,63 @@ const UserTable = () => {
       });
     }
   };
+
   return (
     <div className="container">
       <div className="heading">
         <div className="heading-text">
-          <h2>User Management System</h2>
+          <h2 className="text-center text-light">User Management System</h2>
         </div>
         <div className="text-center mb-3">
-          <Link
-            to={"/user/create"}
-            className="btn btn-add btn-primary "
-          >
+          <Link to={"/user/create"} className="btn btn-add btn-primary ">
             Add User
           </Link>
         </div>
       </div>
-      {users.length ? (
-        users.map((user, index) => (
-          <div className="col-sm-12 mb-3 user-card" key={index}>
-            <div className="card">
-              <div className="card-body ">
-                <div className="d-flex justify-content-between">
-                  <h3 className="card-title ">
-                    {user.firstname} {user.lastname}
-                  </h3>
-                  <div>
-                    <button
-                      className="btn btn-primary ms-1"
-                      onClick={() => editDetails(user._id)}
-                    >
-                      <i className="bi bi-pencil-square"></i>
-                    </button>
-                    <button
-                      className="btn btn-danger ms-1"
-                      onClick={() => removeDetails(user._id)}
-                    >
-                      <i className="bi bi-trash"></i>
-                    </button>
+      {loader ? (
+        <div className="d-flex  shimmer-card ">
+          <Shimmer /> <Shimmer /> <Shimmer /> 
+        </div>
+      ) : (
+        <div className="card-container d-flex flex-wrap ">
+          {users.length ? (
+            users.map((user, index) => (
+              <div className=" mb-3 user-card rounded col-sm-4 " key={index}>
+                <div className="card mx-2">
+                  <div className="card-body  ">
+                    <div className="d-flex  justify-content-between">
+                      <h3 className="card-title ">
+                        {user.firstname} {user.lastname}
+                      </h3>
+                      <div className="d-flex h-50">
+                        <button
+                          className="btn btn-primary ms-1"
+                          onClick={() => editDetails(user._id)}
+                        >
+                          <i className="bi bi-pencil-square"></i>
+                        </button>
+                        <button
+                          className="btn btn-danger ms-1"
+                          onClick={() => removeDetails(user._id)}
+                        >
+                          <i className="bi bi-trash"></i>
+                        </button>
+                      </div>
+                    </div>
+                    <p className="card-text mb-1">+91 {user.phone}</p>
+                    <p className="card-text  ">{user.email}</p>
                   </div>
                 </div>
-                <p className="card-text mb-1">+91 {user.phone}</p>
-                <p className="card-text ">{user.email}</p>
               </div>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p>No User Added</p>
+            ))
+          ) : (
+            <p className="text-light">No User Added</p>
+          )}
+        </div>
       )}
     </div>
   );
 };
 
 export default UserTable;
-// <div classNameName="table-container">
-//   <table>
-//     <thead>
-//       <tr>
-//         <th>First Name</th>
-//         <th>Last Name</th>
-//         <th>Email</th>
-//         <th>Phone</th>
-//         <th>Actions</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//       {users &&
-//         users.map((user, index) => (
-//           <tr key={index}>
-//             <td>{user?.firstname}</td>
-//             <td>{user?.lastname}</td>
-//             <td>{user?.email}</td>
-//             <td>{user?.phone}</td>
-//             <td>
-//               <button
-//                 classNameName="btn btn-primary m-3 btn-sm"
-//                 onClick={() => editDetails(user._id)}
-//               >
-//                 Edit
-//               </button>
-//               <button
-//                 classNameName="btn btn-danger m-3 btn-sm"
-//                 onClick={() => removeDetails(user._id)}
-//               >
-//                 Delete
-//               </button>
-//             </td>
-//           </tr>
-//         ))}
-//     </tbody>
-//   </table>
-// </div>
+
